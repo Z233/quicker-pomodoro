d052d48f10f24f1b5e85d4b6bc7aede1a0463db3
z233 <z233@msn.com>
2022-11-04 00:13:37 +0800

    chore: improve style

--- src/components/StartButton.svelte
+++ src/components/StartButton.svelte
@@ -4,7 +4,7 @@
 
 <button
   class="flex bg-sky-700 text-white px-4 py-2 rounded-md hover:opacity-95 
-  shadow-[0_4px] shadow-sky-900 active:translate-y-[4px] active:shadow-none 
-  transition duration-75 border-b-2 border-sky-900 drop-shadow-md"
+  shadow-[0_3px] shadow-sky-900 active:translate-y-[3px] active:shadow-none 
+  transition duration-75 border-b border-sky-900 drop-shadow-md"
   ><span class="mr-1"><PlayOutline /></span> START
 </button>
