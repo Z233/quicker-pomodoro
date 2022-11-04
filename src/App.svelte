971381742a7b4787b634816d9be5894dd31fa5b4
z233 <z233@msn.com>
2022-11-04 11:51:09 +0800

    feat: basic function

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,51 @@
 <script>
   import StartButton from './components/StartButton.svelte'
   import TimeInput from './components/TimeInput.svelte'
+
+  const STEP = 15
+
+  let mins = 30
+  $: endTimeString = getEndTimeString(mins)
+
+  function getEndTimeString(mins) {
+    const endTimestamp = Date.now() + mins * 60 * 1000
+    const date = new Date(endTimestamp)
+    const endHours = date.getHours()
+    const endMins = date.getMinutes()
+    return `${formatTime(endHours)}:${formatTime(endMins)}`
+  }
+
+  function formatTime(value) {
+    return value < 10 ? `0${value}` : value
+  }
+
+  let handleIncrese = () => {
+    mins += STEP
+  }
+
+  let handleDecrese = () => {
+    const tempMins = mins - STEP
+    if (tempMins > 0) {
+      mins = tempMins
+    }
+  }
+
+  let handleStart = () => {
+    // @ts-ignore
+    window.start(mins)
+  }
 </script>
 
 <main class="grid place-content-center h-screen w-screen">
   <div class="flex flex-col items-center space-y-8">
-    <TimeInput />
-    <StartButton />
+    <TimeInput
+      bind:mins
+      bind:onIncrease={handleIncrese}
+      bind:onDecrease={handleDecrese}
+    />
+    <div class="text-sm text-gray-800">
+      从现在起，一直到 <span class="font-bold"> {endTimeString}</span>。
+    </div>
+    <StartButton bind:onStart={handleStart} />
   </div>
 </main>
--- src/components/TimeInput.svelte
+++ src/components/TimeInput.svelte
@@ -1,6 +1,10 @@
 <script>
   import ArrowDown from '../icons/ArrowDown.svelte'
   import ArrowUp from '../icons/ArrowUp.svelte'
+
+  export let mins
+  export let onIncrease = () => {}
+  export let onDecrease = () => {}
 </script>
 
 <div
@@ -9,18 +13,21 @@
   <div
     class="h-full border-r border-gray-200 flex flex-col justify-center text-center px-10"
   >
-    <span class="text-3xl">30 </span><span
-      class="text-xs text-gray-400"
-    >
-      mins
-    </span>
+    <div class="text-3xl w-12">{mins}</div>
+    <span class="text-xs text-gray-400"> 分钟 </span>
   </div>
   <div class="h-full grid">
-    <button class="px-2 grid place-content-center hover:bg-gray-50">
+    <button
+      on:click={onIncrease}
+      class="px-2 grid place-content-center 
+      hover:bg-gray-100 hover:bg-opacity-50 active:bg-opacity-100"
+    >
       <ArrowUp />
     </button>
     <button
-      class="px-2 border-t border-gray-200 grid place-content-center  hover:bg-gray-50"
+      on:click={onDecrease}
+      class="px-2 border-t border-gray-200 grid place-content-center  
+      hover:bg-gray-100 hover:bg-opacity-50 active:bg-opacity-100"
     >
       <ArrowDown />
     </button>
